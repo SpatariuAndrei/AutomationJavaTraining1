4699a3019ad11b825bc8a9ec5abb3b83c79b3975
Raul Beniamin Sicoie <Raul.Sicoie@endava.com>
2019-11-11 10:16:43 +0200

    first tutorial done

--- RaulS/src/test/java/base/BaseTests.java
+++ RaulS/src/test/java/base/BaseTests.java
@@ -1,22 +1,36 @@
 package base;
 
-import org.openqa.selenium.WebDriver;
+import com.google.common.io.Files;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+import utils.CookieManager;
+import utils.EventReporter;
 import utils.WindowManager;
+
+import java.io.File;
+import java.io.IOException;
+
 
 public class BaseTests {
 
-    private WebDriver driver;
+    private EventFiringWebDriver driver;
     protected HomePage homePage;
 
     @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "C:/Work/cld_automation_community/RaulS/src/main/resources/chromedriver.exe");
-        driver = new ChromeDriver();
+        driver = new EventFiringWebDriver(new ChromeDriver(getChromeOptions()));
+        driver.register(new EventReporter());
         navigateToHomePage();
     }
 
@@ -31,7 +45,32 @@ public class BaseTests {
         driver.quit();
     }
 
-    public WindowManager getWindowManager(){
+    @AfterMethod
+    public void recordFailureScreenshot(ITestResult result) {
+        if (ITestResult.FAILURE == result.getStatus()) {
+            TakesScreenshot camera = (TakesScreenshot) driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.move(screenshot, new File("C:/Work/cld_automation_community/RaulS/src/main/resources/" + result.getName() + ".png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public WindowManager getWindowManager() {
         return new WindowManager(driver);
     }
+
+    private ChromeOptions getChromeOptions() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("disable-infobars"); //disabling the info bar
+        return chromeOptions;
+    }
+
+    public CookieManager getCookieManager(){
+        return new CookieManager(driver);
+    }
+
 }
