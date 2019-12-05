900a4c7868ea00811042c4b2fcabb726aa6778a2
Raul Beniamin Sicoie <Raul.Sicoie@endava.com>
2019-12-05 16:27:26 +0200

    removed qqq from searchbar

--- rauljbehave/src/main/java/pages/HomePage.java
+++ rauljbehave/src/main/java/pages/HomePage.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.assertEquals;
 
 public class HomePage extends LoadableComponent {
 
-    @FindBy(id = "searchboxTriggerQQQQQQQ")
+    @FindBy(id = "searchboxTrigger")
     private WebElement searchBar;
 
     @FindBy(xpath = "//button[@class='btn btn-default searchbox-submit-button']")
