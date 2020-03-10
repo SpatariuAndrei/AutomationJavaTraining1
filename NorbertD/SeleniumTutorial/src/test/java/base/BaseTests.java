374756643ad575904bb74f9ec57ad9ca76cd63da
Norbert Deak <Norbert.Deak@endava.com>
2020-03-10 14:57:07 +0200

    Selenium Chapter 4

--- NorbertD/SeleniumTutorial/src/test/java/base/BaseTests.java
+++ NorbertD/SeleniumTutorial/src/test/java/base/BaseTests.java
@@ -1,32 +1,27 @@
 package base;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.List;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pages.HomePage;
 
 public class BaseTests {
 
     private WebDriver driver;
+    protected HomePage homePage;
 
+    @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
+        homePage = new HomePage(driver);
+    }
 
-//        List<WebElement> links = driver.findElements(By.tagName("a"));
-//        System.out.println(links.size());
-//
-//        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
-//        inputsLink.click();
-
-        driver.findElement(By.linkText("Shifting Content")).click();
-        driver.findElement(By.linkText("Example 1: Menu Element")).click();
-        System.out.println(driver.findElements(By.tagName("li")).size());
-
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
     }
 
     public static void main(String args[]){
