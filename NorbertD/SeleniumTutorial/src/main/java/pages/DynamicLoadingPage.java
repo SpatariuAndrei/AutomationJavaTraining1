e25f17714a75e4645f28a8e7151b996a8a5ebbcf
Norbert Deak <Norbert.Deak@endava.com>
2020-03-11 17:24:22 +0200

    Selenium Chapter 9

---
+++ NorbertD/SeleniumTutorial/src/main/java/pages/DynamicLoadingPage.java
@@ -0,0 +1,21 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class DynamicLoadingPage {
+
+    private WebDriver driver;
+    private By link_Example2 = By.xpath("//*[@id=\"content\"]/div/a[2]");
+
+    public DynamicLoadingPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public DynamicLoadingExample2Page clickOnExample2Link(){
+        driver.findElement(link_Example2).click();
+        return new DynamicLoadingExample2Page(driver);
+    }
+}
---
+++ NorbertD/SeleniumTutorial/src/test/java/wait/WaitTests.java
@@ -0,0 +1,17 @@
+package wait;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class WaitTests extends BaseTests {
+
+    @Test
+    public void testWaitUntilVisible(){
+        var loadingPage = homePage.click().clickOnExample2Link();
+        loadingPage.clickOnStartButton();
+        assertTrue(loadingPage.isTextPresent());
+    }
+
+}
