0118f028d7b46fd12ac5a0c9cf1d5ac8a7c71c65
Vvarga <vasile.varga@endava.com>
2020-04-13 23:42:10 +0300

    Some more practice files added

---
+++ VasiVarga/webdriver_java/src/test/java/dropdown/DropdownTest.java
@@ -0,0 +1,24 @@
+package dropdown;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class DropdownTest extends BaseTests {
+
+    @Test
+    public void testSelectOption(){
+        var dropDownPage = homepage.clickDropdown();
+
+        String option = "Option 1";
+
+        dropDownPage.selectFromDropdown(option);
+
+        var selectedOptions = dropDownPage.getSelectedOptions();
+        assertEquals(selectedOptions.size(),1,"Incorrect number of selections");
+        assertTrue(selectedOptions.contains(option), "Option not selected");
+    }
+
+}
