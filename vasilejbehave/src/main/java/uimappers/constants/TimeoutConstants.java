89bd42e54c38cac241ed763083aa57d896a23f60
SpatariuAndrei <50962467+SpatariuAndrei@users.noreply.github.com>
2021-02-28 16:20:38 +0200

    Delete vasilejbehave directory

--- vasilejbehave/src/main/java/uimappers/constants/TimeoutConstants.java
+++
@@ -1,7 +0,0 @@
-package uimappers.constants;
-
-public class TimeoutConstants {
-    public final static int PAGE_LOADING_TIMEOUT = 60;
-    public final static int DEFAULT_TIMEOUT =30;
-    public final static int MIN_TIMEOUT = 5;
-}
